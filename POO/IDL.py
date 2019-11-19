168d72e27d3407ced128dc3dcaa7adbd36615002
agustinlezcano <50871051+agustinlezcano@users.noreply.github.com>
2019-11-19 11:12:12 -0300

    Update IDL.py

--- POO/IDL.py
+++ POO/IDL.py
@@ -1,53 +1,163 @@
-#Este clase permite de analizar cada data que viene del servidor
+Este clase permite analizar cada data que viene del servidor
 #Este data he analizado y despues he tratado en la clase Comandos 
-#La classe
+#La clase
 import sys
+sys.path.insert(0, 'home/agustin/Proyecto_poo/Proyecto_poo/POO')
+import Leer as lectura
+import trayectoria as trayec
+import Articulacion as ef
 class IDL:
-    def __init__(self, auto):
+    def __init__(self):
         #self.cmd = cmd
         self.liste = []
-        self.auto = auto #bool para saber si modo auto o modo manual
 
-    def analizarReporte_auto(self, cmd, auto): #por el modo automatico
-        if(auto):
-            switcher = {
-                "girar": 'Girar', #poner los letras requiridos
-                "mover": 'Mover',
-                "parar": 'Parar',
-                #ect / cada opciones del robot
-            }
-            self.liste.append(switcher.get(cmd, "Error"))
-        return(self.liste, auto)
 
-    def analizarReporte_manu(self, cmd, auto, b_arti):
-        b_arti = False #poner este estado si el reporte empienza con Arti
-        if(auto != True):
-            switcher = {
-                "A": '1', #poner los letras requiridos
-                "B": '2',
-                "C": '3',
-                "operarefector": 'operarEfector',
-                "moverPosicionOrigen" : 'moverPosicionOrigen',
-                "moverarticulaciones" : 'MoverArticulaciones',
-                #ect
-            }
-            self.liste.append(switcher.get(cmd, "Error"))
-        return(self.liste, b_arti) 
 
-    def otroscomandos(self, cmd):
-        switcher = {
-                "salir": 'salir', #poner los letras requiridos
-                "?": '?',
-                "help": 'help',
-                "activar": 'Activar',#ect
-                "desactivar": 'Desactivar',
-                "conexion" : 'Conexion',
-                "deconexion" : 'Deconexion',
-            }
-        self.liste.append(switcher.get(cmd, "Error"))
-        return self.liste 
+    def recibirOrden(self, orden):
+        if (orden[0]=='E' and orden[len(orden)-1]=='P'):
+            for i in orden:
+                if orden[i]=='A' or 'B' or orden[i]=='A':
+            
+					
 
-if __name__ == "__main__":
-    auto = True
-    a = IDL(auto)
-    print(a.analizarReporte_auto("girar", auto))
+    def __main__(self):
+        print("==========================================="
+              "             ORDENES             "   
+            "1 - Control articulaciones"  
+            "2 - Control efector final"  
+            "0 - Salir"
+            "===========================================")
+        band = True
+        while (band==True) :
+            ej=input( "Ingrese el número de orden a realizar: ")
+            if (ej == 0):
+                band =False
+                break
+            if (ej == 1):
+                IDL.controlBrazo()
+                break 
+            if (ej == 2):
+                IDL.controlPinza()
+                break
+
+
+def controlBrazo(self):
+    
+   
+    modo=input("Ingrese modo:"   
+    "A - Modo Automático"  
+    "M - Modo Manual" )
+    
+    #Aca solo llama a las otras clases
+    #Trayectoria * trayec= new Trayectoria()
+    #Leer * lectura = new Leer()
+    
+    #string orden
+    if (modo == "A"):
+        #string archivo
+        status = True #Inactivo
+        while (status==True):
+            aux=0
+            aux=input("1. Cargar rutina \n2. Generar rutina de aprendizaje")
+            print( "\n")
+            if (aux==2):
+                lectura.crear_rutina()
+            
+            if (aux==1):
+                archivo=input( "Ingrese archivo para leer: ")
+                orden=lectura.read(archivo)
+            
+                if (orden!="ERROR"):
+                    status = 0
+                
+            
+        
+    
+    
+    
+    while(True):
+        if (modo == "M"):
+            orden=input("Ingrese el número de la acción:"   
+            "E - Empezar"  
+            "P - Parar"  
+            "A - Articulacion A"  
+            "B - Articulacion B"  
+            "C - Articulacion C"  
+            "T para terminar") 
+            print(orden) 
+        
+        
+        if(orden=="T"):
+            break
+        
+       
+        if(trayec.esOrdenValida(orden)):
+            print( "Orden válida")  
+        
+        if (modo == "A"):
+            orden="T"
+        
+   
+
+
+def controlPinza(self) :
+    
+    
+    #Leer * lectura = new Leer()
+    
+    #string orden, archivo
+    #string inicializacion
+    
+    
+    inicializacion=input( "¿Desea inicializar? S / N:  ") 
+   
+    if (inicializacion == "s" or inicializacion == "S") :
+	ef.Inicio()
+    else :
+	print( "Saliendo...") 
+	ef.setEstado(False) # Falso activo
+    
+    
+    modo=input("Ingrese modo:"   
+                "A - Modo Automático"  
+                 "M - Modo Manual") 
+    
+    
+    if(modo == "A"):
+        archivo=input( "Ingrese archivo para leer: ")
+        orden=lectura.read(archivo)
+        
+        if(ef.esOrdenValida(orden)):
+            print( "Orden válida" ) 
+        
+    
+    
+    if (modo == "M"):
+    
+        while (ef.getEstado()) :
+            orden=input( "Elija la acción a realizar: "  
+                     "E para Empezar"  
+                     "P para Parar"  
+                     "A+ para Abrir"  
+                     "A- para Cerrar"  
+                     "R para Rotar"  
+                     "V para Cambiar velocidad"  
+                     "T para Terminar") 
+            
+            if(orden=="T"):
+                #delete ef
+                break
+            
+            
+            if(ef.esOrdenValida(orden)):
+                print( "Orden válida")  
+                #delete ef
+            
+            if (modo == "A"):
+                orden="T"
+            
+         
+    
+  
+    
+
