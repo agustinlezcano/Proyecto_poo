44b1cd0ed050aae5b7776c855bc08260bc1808b8
Agustin Lezcano <lezcanoagustin2012@gmail.com>
2019-11-19 13:33:51 -0300

    Agregada la clase tiempo y corregida articulacion

---
+++ POO/articulacion.py
@@ -0,0 +1,104 @@
+
+
+class Articulacion:
+    def __init__(self):
+        #super().__init__()
+		self.ciclos_totales
+		self.velocidad = 0
+		self.Estado # Activo = FALSE Inactivo = TRUE
+		self.Estadostr
+		self.Operacion    
+        self.anguloGiro
+		
+    def Inicio(self):
+	    Articulacion.setEstado(True) # Inactivo
+	    print ("Estado del efector final: ",Articulacion.getEstadostr())    #Arreglar esto
+	    self.ciclos_totales = 0
+    
+	
+    def Abrir(self, actual, counter) :
+	    Articulacion.setEstado(False) # Activo
+	    Operacion = "Abrir" #Abre sus pinzas
+	    if (actual + 1 == counter) :
+		    Articulacion.setEstado(True) # Inactivo
+	    self.ciclos_totales+=1
+
+
+    def Cerrar(self,actual,  counter) :
+	    Articulacion.setEstado(False) # Activo
+	    Operacion = "Cerrar" #Cierra sus pinzas
+	    if (actual + 1 == counter) :
+		    Articulacion.setEstado(True) # Inactivo
+	    self.ciclos_totales+=1
+
+
+    def Rotar(self,actual, counter) :
+	    Articulacion.setEstado(False) # Activo
+	    Operacion = "Rotar" 
+	    if (actual + 1 == counter) :
+		    Articulacion.setEstado(True) # Inactivo	
+	    self.ciclos_totales+=1
+
+
+    def Cambiar_Velocidad(self, Nueva_velocidad, actual, counter):
+	    Articulacion.setEstado(False) # Activo
+	    Operacion = "Cambiar Velocidad"
+	    velocidad = Nueva_velocidad
+	    if (actual + 1 == counter) :
+		    Articulacion.setEstado(True) # Inactivo
+	    self.ciclos_totales+=1
+
+
+    def getvelocidad(self) :
+        return Articulacion.velocidad
+
+
+    def getEstado(self) :
+	    return Articulacion.Estado
+
+
+    def setEstado(self, Nuevo_Estado) :
+	    Articulacion.Estado = Nuevo_Estado
+
+
+    def getEstadostr(self) :
+	if (Articulacion.Estado == True) :
+		return ("INACTIVO (libre)")
+	else :
+		return ("ACTIVO (en proceso)")
+	
+
+
+    def getciclos(self) :
+	    return Articulacion.ciclos_totales
+
+
+    def sumarciclos(self) :
+	    Articulacion.ciclos_totales+=1
+
+
+    def getOperacion(self) :
+	    return Articulacion.Operacion
+
+
+    def parada(self) :
+	        Articulacion.setEstado(False) # Activo
+
+		
+	def esOrdenValida(self, x):
+        valido= False
+        if (x[0]=='E' and x[len(x)-1]=='P'):
+            print("es cierto que hay e o p")
+            if ('A' in x or 'B' in x or 'C' in x):
+                if ((x[x.index('A')+1]=='+' or x[x.index('A')+1]=='-')  and (x[x.index('V')+1]=='+' or x[x.index('V')+1]=='-') and (x[x.index('R')+1]=='+' or x[x.index('R')+1]=='-')):
+                    print("Hay un signo de + o de -")
+                    if ((1<=abs((x.index('A')+1)-x.index('V'))<=4) and (1<=abs((x.index('V')+1)-x.index('R'))<=4) and (1<=abs((x.index('R')+1)-x.index('P'))<=4)):
+                        # or (1<=abs(x.index('A')-x.index('C'))<=4 and (1<=(x.index('B')-x.index('C'))<=4)): # 
+                        print("anda")
+                        #Llamar a la funcion para variar el ángulo
+                        #Las tres variables de abajo probablemente no hagan falta
+                        valido=True
+                        return (valido)
+                    else:
+                        print("Comando inválido") 
+                        return(valido) 
---
+++ POO/tiempo.py
@@ -0,0 +1,46 @@
+import time
+
+class Tiempo(self):
+    def __init__(self):
+        #super().__init__()
+        self.time0
+        self.time
+        self.timeOp
+        self.timeOrdenes
+
+    
+    def getTime(self) :
+        return Tiempo.time
+    
+
+    def setTime(self, new_time) :
+        Tiempo.time = new_time
+    
+
+    def getTime0(self) :
+        return Tiempo.time0
+    
+
+    def setTime0(self, new_time) :
+        Tiempo.time0 = new_time
+    
+
+    def medirTime(self):
+        t = time.time()
+        ss=[]       #Vector para guardar los tiempos
+        Tiempo.setTime(t)     
+        segundos = ((Tiempo.getTime() - Tiempo.getTime0())) #/ CLOCKS_PER_SEC)
+        ss.append("Instante de inicio orden: ", segundos, " seg") 
+        print(ss) 
+    
+
+    def setActivityTime(self, t):
+        timeOp = t+1
+    
+
+    def setOrdenesTime(self, t):
+        Tiempo.timeOrdenes.append(t)
+        
+
+
+
